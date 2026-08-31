c08c4f6cf28dcd444114e8737dd0506ac72f6a76
Mohd-kaif123 <mansoorikaif365@gmail.com>
2026-08-31 19:15:21 +0530

    image upload to s3 Bucket

---
+++ Cloud-Front-web/cloudfront_web/main.tf
@@ -0,0 +1,151 @@
+# This is the main Terraform configuration file for setting up AWS CloudFront distribution and related resources.
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>6.0"
+    }
+  }
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create an S3 bucket 
+resource "aws_s3_bucket" "website" {
+  bucket = "kaif-cloudfront-website-29082026"
+  tags = {
+    Name        = "website"
+    Environment = "Dev"
+  }
+}
+
+# Create an S3 bucket public access block to prevent public access
+resource "aws_s3_bucket_public_access_block" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+# Create an S3 bucket policy to allow public read access to the bucket
+resource "aws_s3_bucket_policy" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  depends_on = [aws_s3_bucket_public_access_block.website]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.website.arn}/*"
+      }
+    ]
+  })
+}
+
+# Enable static website hosting on the S3 bucket
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+# upload index.html and error.html to the S3 bucket
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.website.id
+  key          = "index.html"
+  source       = "index.html"
+  content_type = "text/html"
+  etag         = filemd5("index.html")
+}
+
+resource "aws_s3_object" "error" {
+  bucket       = aws_s3_bucket.website.id
+  key          = "error.html"
+  source       = "error.html"
+  content_type = "text/html"
+  etag         = filemd5("error.html")
+}
+
+# Create CloudFront distribution for the S3 bucket
+resource "aws_cloudfront_distribution" "website" {
+
+  enabled = true
+
+  origin {
+
+    domain_name = aws_s3_bucket_website_configuration.website.website_endpoint
+
+    origin_id = "S3-${aws_s3_bucket.website.id}"
+
+    custom_origin_config {
+
+      http_port  = 80
+      https_port = 443
+
+      origin_protocol_policy = "http-only"
+
+      origin_ssl_protocols = [
+        "TLSv1.2"
+      ]
+    }
+  }
+
+
+  default_cache_behavior {
+
+    allowed_methods = [
+      "GET",
+      "HEAD"
+    ]
+
+    cached_methods = [
+      "GET",
+      "HEAD"
+    ]
+
+    target_origin_id = "S3-${aws_s3_bucket.website.id}"
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+
+  default_root_object = "index.html"
+
+
+  restrictions {
+
+    geo_restriction {
+
+      restriction_type = "none"
+    }
+  }
+
+
+  viewer_certificate {
+
+    cloudfront_default_certificate = true
+  }
+}
---
+++ Image-Upload-To-S3_Bucket/flask-s3-terraform/main.tf
@@ -0,0 +1,119 @@
+## terraform Configuration for deploying a Flask application to AWS S3
+terraform {
+  required_providers {
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+# Create EC2 instance to host the Flask application
+# Install Python, Flask, and other dependencies on the EC2 instance
+# add User data in --> app.py
+# Run the Application on the EC2 instance
+# open the Ports  80, 5000
+
+resource "aws_security_group" "aws_sg" {
+  name = "ec2-sg"
+  description = "Allow port 80 and 5000"
+  vpc_id = "vpc-0ca1f92ea561a49d6"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+  ingress {
+    from_port = 5000
+    to_port = 5000
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+}
+
+resource "aws_instance" "my_ec2" {
+  instance_type = "t2.micro"
+  ami = "ami-004f790b835b26145"
+  subnet_id = "subnet-027f44b03c5dc995a"
+  user_data = <<-EOF
+  #!/bin/bash
+  sudo apt update -y
+  sudo apt install python3 -y
+  sudo apt install python3 python3-flask python3-boto3
+
+  # app.py file create kar rahe hai
+  cat << 'APPFILE' > /home/ubuntu/app.py
+  from flask import Flask
+  import boto3
+
+  app = Flask(__name__)
+
+  @app.route('/')
+  def home():
+    return "Flask App Running on EC2 with Boto3!"
+  
+  if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+  APPFILE
+
+  # Background me Flask App run karna
+  nohup python3 /home/ubuntu/app.py > /home/ubuntu/app.log 2>&1 &
+  EOF
+
+
+  
+
+}
+
+# s3 bucket to host the Flask application
+resource "aws_s3_bucket" "flask_app_bucket"{
+  bucket = "kaif-s3-bucket-30082026"
+  tags = {
+    Name        = "FlaskAppBucket"
+    Environment = "Development"
+    project     = "FlaskApp"
+  }
+}
+
+# s3 bucket policy s3:PutObject for the Flask application bucket
+# s3 bucket plicy s3:GetObject for the Flask application bucket
+resource "aws_s3_bucket_policy" "flask_app_bucket_policy" {
+
+  bucket = aws_s3_bucket.flask_app_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Sid    = "AllowFlaskGetPut"
+        Effect = "Allow"
+
+        Principal = {
+          AWS = "arn:aws:iam::748810634002:user/Nikunj"
+        }
+
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ]
+
+        Resource = "${aws_s3_bucket.flask_app_bucket.arn}/*"
+      }
+    ]
+  })
+}
